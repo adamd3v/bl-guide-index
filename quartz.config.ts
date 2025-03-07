2fb96f448c566927745743442dd9c2df27123095
Fizzy <21183059+Fizzyhex@users.noreply.github.com>
2025-03-07 20:54:12 +0000

    Disabled SPA :(

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "👥 BONELAB Guide Index",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
